f3d765884f34d1f21c611e55d32cf757fdb9879e
vankoosh <vankoosh@gmail.com>
2023-01-09 10:25:06 +0000

    next test

--- components/Front.jsx
+++ components/Front.jsx
@@ -2,13 +2,21 @@ import Image from "next/image";
 
 export default function Front() {
   return (
-    <div className="relative w-full h-screen">
-      <Image
-        src="/imgs/barn-door-hero.webp"
-        alt="hero image"
-        fill
-        quality="100"
-      />
-    </div>
+    <>
+      <div className="relative w-full h-screen">
+        <Image
+          src="/imgs/barn-door-hero.webp"
+          alt="hero image"
+          fill
+          quality="100"
+        />
+      </div>
+      <div className="z-40 mt-32 text-black absolute top-[20vh] left-[50vw]">
+    <h1>MODERNÉ</h1>
+    <h1>&nbsp;&nbsp;ELEGANTNÉ</h1>
+    <h1>&nbsp;&nbsp;&nbsp;&nbsp;NEPREHLIADNUTEĽNÉ</h1>
+    <a href="">INŠPIRUJTE SA...</a>
+  </div>
+    </>
   );
 }
